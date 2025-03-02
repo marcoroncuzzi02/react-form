e4435dac0a7340eb48b1988a4b1e6317de91d3f2
marcoroncuzzi02 <marcoroncuzzi02@gmail.com>
2025-03-02 21:33:06 +0100

    ADD some logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import languages from "./languages.js"
+import { useState } from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [name,setName] = useState('')
+  const [list, setList] = useState(languages);
+
+  const addName = e => {
+    e.preventDefault();
+    setList([...list, name]);
+    // list.push(name);
+    }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>list</h1>
+      <form onSubmit={addName}>
+        <input 
+        type="text"
+        value={name}
+        onChange={e => { setName(e.target.value)}} />
+        <button>invio</button>
+      </form>
+
+      <ul className=" containere d-flex flex-column">
+      {list.map( (element) =>{
+                return(
+                  <li
+                    className="list-element"
+                    key={element.id}
+                  >
+                    {element.title}
+                  </li>
+                )
+              }      
+      )}
+      </ul>
+            
     </>
   )
 }
